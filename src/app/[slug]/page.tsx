79d1bca56f9e9a664d751f8f4e328efa6ccabef7
eacastel <emilio@aliigo.com>
2025-05-14 04:24:16 -0700

    Fix metadata app slug page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -19,9 +19,9 @@ import type {
 export async function generateMetadata({
   params,
 }: {
-  params: { slug: string };
+  params: Promise<{ slug: string }>;
 }): Promise<Metadata> {
-  const { slug } = params; 
+  const { slug } = await params; 
   const page = await getPage(slug);
   const image = page?.fields?.previewImage as Asset;
 
