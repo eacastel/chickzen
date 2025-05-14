60f78d40eb86ef5f43fb963b2a70b2cadc956a8b
eacastel <emilio@aliigo.com>
2025-05-14 13:27:50 -0700

    update rich text renderer

--- src/components/RichTextRenderer.tsx
+++ src/components/RichTextRenderer.tsx
@@ -1,11 +1,48 @@
-// components/RichTextRenderer.tsx
+"use client";
+
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import type { Document } from "@contentful/rich-text-types";
+import {
+  BLOCKS,
+  INLINES,
+  type Document,
+} from "@contentful/rich-text-types";
 
 export default function RichTextRenderer({ document }: { document: Document }) {
   return (
-    <div className="prose prose-lg max-w-none mx-auto text-justify">
-      {documentToReactComponents(document)}
+    <div className="prose prose-lg max-w-none">
+      {documentToReactComponents(document, {
+        renderNode: {
+          [BLOCKS.HEADING_1]: (node, children) => (
+            <h1>{children}</h1>
+          ),
+          [BLOCKS.HEADING_2]: (node, children) => (
+            <h2>{children}</h2>
+          ),
+          [BLOCKS.HEADING_3]: (node, children) => (
+            <h3>{children}</h3>
+          ),
+          [BLOCKS.PARAGRAPH]: (node, children) => (
+            <p>{children}</p>
+          ),
+          [BLOCKS.UL_LIST]: (node, children) => (
+            <ul>{children}</ul>
+          ),
+          [BLOCKS.OL_LIST]: (node, children) => (
+            <ol>{children}</ol>
+          ),
+          [BLOCKS.LIST_ITEM]: (node, children) => (
+            <li>{children}</li>
+          ),
+          [INLINES.HYPERLINK]: (node, children) => {
+            const url = node.data.uri;
+            return (
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                {children}
+              </a>
+            );
+          },
+        },
+      })}
     </div>
   );
 }
